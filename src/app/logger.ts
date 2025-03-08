f47401c32e90360f6a7e39fd2e887ea7eda3ecfa
Farid iskandar <145176951+frdiskandr@users.noreply.github.com>
2025-03-08 16:56:44 +0700

    Update logger.ts

--- src/app/logger.ts
+++ src/app/logger.ts
@@ -6,11 +6,11 @@ const Logger = winston.createLogger({
     defaultMeta: { service: "user-service" },
     transports: [
         new winston.transports.Console(),
-        // new winston.transports.File({
-        //     filename: "./logs/error.log",
-        //     level: "error",
-        // }),
-        // new winston.transports.File({ filename: "./logs/combined.log" }),
+        new winston.transports.File({
+            filename: "/tmp/logs/error.log",
+            level: "error",
+        }),
+        new winston.transports.File({ filename: "./logs/combined.log" }),
     ],
 });
 
