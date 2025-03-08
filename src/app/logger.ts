7927146539b0697c2c14e6d33ea3e2b7ebb4203a
Farid iskandar <145176951+frdiskandr@users.noreply.github.com>
2025-03-08 15:50:17 +0700

    Update logger.ts

--- src/app/logger.ts
+++ src/app/logger.ts
@@ -8,11 +8,11 @@ const Logger = winston.createLogger({
     handleRejections: true,
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({
-            filename: "./logs/error.log",
-            level: "error",
-        }),
-        new winston.transports.File({ filename: "./logs/combined.log" }),
+        // new winston.transports.File({
+        //     filename: "./logs/error.log",
+        //     level: "error",
+        // }),
+        // new winston.transports.File({ filename: "./logs/combined.log" }),
     ],
 });
 
